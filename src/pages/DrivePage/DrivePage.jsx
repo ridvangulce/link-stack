2a4f34ab5290b38464ecf200c88f3806f6a2d268
ridvangulce <ridvangulce@gmail.com>
2023-04-28 06:30:34 +0300

    +fixed bugs about loaded images

--- src/pages/DrivePage/DrivePage.jsx
+++ src/pages/DrivePage/DrivePage.jsx
@@ -27,7 +27,7 @@ const DrivePage = () => {
             token: "ya29.a0Ael9sCMwNHu5cPG9-sgosaDQf9iBeek1qRUm46jxG2vzFyozBa0jcRwl6sNkyqxRKOzkcunCXucRLEwNng1ZxkQwaZ9KhDd7V3miHjHQpVDSkPjx2dO4414Ok9wlNJa6XX_Wjn8HMVyXtRVaC1vdioCJZs5-aCgYKAaYSARISFQF4udJhep8wAzaS0FbJoW7i1kvZSQ0163",
             showUploadFolders: true,
             supportDrives: true,
-            multiselect: false,
+            multiselect: true,
             scope: [
                 "https://www.googleapis.com/auth/drive.file",
                 "https://www.googleapis.com/auth/userinfo.profile",
@@ -41,45 +41,16 @@ const DrivePage = () => {
                     console.log('User clicked cancel/close button')
                 } else if (data.docs && data.docs[0]) {
                     const selectedFile = data.docs[0];
-                    const storageRef = ref(storage, selectedFile.name);
-                    const downloadUrl = selectedFile.downloadUrl;
                     const fileId = selectedFile.id;
-                    const shareableLink = `https://drive.google.com/uc?export=view&id=${fileId}`;
+                    let shareableLink;
+                    shareableLink = `https://drive.google.com/uc?export=view&id=${fileId}`;
                     setSelectedFileUrl(shareableLink);
 
                     console.log(shareableLink);
-                    //-------------> Download the file <--------------
 
-                    const downloadFile = async (url) => {
-                        try {
-                            const response = await fetch(url);
-                            const data = await response.blob();
-                            console.log(data);
-
-                            return data;
-                        } catch (error) {
-                            console.log("ERRORR!:", error);
-                        }
-                    };
-                    //-------------> UPLOAD FILES <------------- 
-
-                    // const blob = await downloadFile(downloadUrl);
-                    // saveAs(blob, selectedFile.name);
                     try {
-                        const metadata = {
-                            contentType: selectedFile.mimeType,
-                        };
-                        await uploadBytes(storageRef, metadata).then((snapshot) => {
-                            console.log("Dosya Başarıyla Yüklendi  " + snapshot.metadata.name);
-                            getDownloadURL(storageRef).then((url) => {
-                                console.log("Dosya URL:  ", url);
-                                setFileUrl(url)
-                            })
-                        });
-                        const downloadURL = await getDownloadURL(storageRef);
-                        await addDoc(collection(db, "images"), { url: downloadURL });
-                        // setSelectedFileUrl(downloadURL);
-                        console.log(selectedFileUrl);
+                        await addDoc(collection(db, "images"), { url: shareableLink });
+                        console.log("Shareable link added to Firestore!");
                     } catch (error) {
                         console.log("ERRORRR!", error);
                     }
@@ -89,20 +60,24 @@ const DrivePage = () => {
             },
         });
     };
+
+
     return (
         <div>
             <button onClick={() => handleOpenPicker()}>Select File</button>
             {selectedFileUrl && (
                 <div>
-                    <p>You selected:{selectedFileUrl}</p>
-                    <img src={selectedFileUrl} alt="Selected file" width="500" height="500" onLoad={() => console.log('Image loaded')} typeof="image/jpeg" />
+                    <p>You selected:</p>
+                    {selectedFileUrl.endsWith(".pdf") ? (
+                        <iframe src={selectedFileUrl} title="Selected file" width="500" height="500" onLoad={() => console.log('Image loaded')} typeof="application/pdf" />
+                    ) : (
+                        <img src={selectedFileUrl} alt="Selected file" width="500" height="500" onLoad={() => console.log('Image loaded')} typeof="image/jpeg" />
+                    )}
                 </div>
             )}
-
-
-
         </div>
-    )
+    );
+
 }
 
 export default DrivePage
